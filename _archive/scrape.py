f39ab892ed1dea580dce7a6fc7ebc715ec757087
n0rgus <github@norgus.com>
2026-01-10 19:22:33 +1100

    Clean-up

--- _archive/scrape.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from dashboard.routes.main import main_bp
-from api.scrape_api_flask import bp as scrape_bp
-
-app = Flask(__name__)
-app.register_blueprint(main_bp)    # page
-app.register_blueprint(scrape_bp)  # API
